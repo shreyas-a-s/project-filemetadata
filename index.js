8974105a3a1b2174e2982ee5a1719024891698a3
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-19 20:11:21 +0530

    feat: Complete index.js with exercise file-metadata functionality

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+'use strict';
+
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -11,8 +16,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch (err) {
+    res.send(400);
+  }
+});
 
-
+app.get('/hello', function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
